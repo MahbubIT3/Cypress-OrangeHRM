c92073311408c7a82511ffaf980c32b435459d8c
Mahabub Alam <mahabubalam.se2018@gmail.com>
2024-01-04 22:46:11 +0600

    Add Punch Out

--- cypress/e2e/OrangeHRM/employee.cy.js
+++ cypress/e2e/OrangeHRM/employee.cy.js
@@ -13,33 +13,56 @@ beforeEach("Employee Login Test",()=>{
     cy.log("Login test completed...")
 });
 
-it("Punch In",()=>{
-    cy.wait(5000)
-    let punchInDate;
-    let punchInTime;
-    cy.get('div.orangehrm-attendance-card-bar > button')
-        .click()
-    cy.wait(10000)
-    cy.get('.oxd-date-input > .oxd-input').invoke('val').then(value=>{
-        punchInDate = value;
-        cy.log(`Punched in Date: ${punchInDate}`);
-        cy.wrap(punchInDate).as('punchInDate');
-    });
 
-    cy.get('.oxd-time-input > .oxd-input').invoke('val').then(value=>{
-        punchInTime = value;
-        cy.log(`Punched in Time: ${punchInTime}`);
-        cy.wrap(punchInTime).as('punchInTime');
-    });
+    it("Punch In or Out",()=>{
+        cy.wait(5000)
+        let punchInDate;
+        let punchInTime;
+        let punchType;
+        cy.get('div.orangehrm-attendance-card-bar > button')
+            .click()
+        cy.wait(10000)
+        cy.get('.orangehrm-card-container > .oxd-text--h6').invoke('text').then((text)=>{
+            punchType = text;
+            if (punchType == "Punch In") {
+                cy.get('.oxd-date-input > .oxd-input').invoke('val').then(value=>{
+                    punchInDate = value;
+                    cy.log(`Punched in Date: ${punchInDate}`);
+                    cy.wrap(punchInDate).as('punchInDate');
+                });
+        
+                cy.get('.oxd-time-input > .oxd-input').invoke('val').then(value=>{
+                    punchInTime = value;
+                    cy.log(`Punched in Time: ${punchInTime}`);
+                    cy.wrap(punchInTime).as('punchInTime');
+                });
+        
+                cy.get('.oxd-button')
+                    .click();
+                cy.wait(5000)
+                cy.get('@punchInDate').then(punchInDate => {
+                    cy.get('@punchInTime').then(punchInTime => {
+                    cy.get(':nth-child(2) > .oxd-text--p')
+                        .should("contain.text", punchInDate)
+                        .should("contain.text", punchInTime);
+                });
+            });
+                cy.log("You are punched in. You can now punch out");
+            } 
+            else {
+                cy.log("Please punch out");
+                cy.get('.oxd-time-input > .oxd-input')
+                    .click()
+                for(let i=0; i<=5; i++){
+                cy.get('.oxd-time-minute-input > .bi-chevron-up')
+                    .click()
+                }
+                cy.get(':nth-child(2) > input')
+                    .click()
+                cy.get('.oxd-button')
+                    .click()
+            }
+        });
+        
 
-    cy.get('.oxd-button')
-        .click();
-    cy.wait(5000)
-    cy.get('@punchInDate').then(punchInDate => {
-        cy.get('@punchInTime').then(punchInTime => {
-          cy.get(':nth-child(2) > .oxd-text--p')
-            .should("contain.text", punchInDate)
-            .should("contain.text", punchInTime);
-    });
-});
 });
