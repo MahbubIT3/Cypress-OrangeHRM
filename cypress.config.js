d24f2ace279412920e070fdc2f5fc1cf346d0531
Mahabub Alam <mahabubalam.se2018@gmail.com>
2024-01-06 22:42:06 +0600

    Generate Test Report

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,22 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  pageLoadTimeout: 10000,
+  watchForFileChanges: false,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
   video: true,
-  screenshotOnRunFailure: true
+  screenshotOnRunFailure: true,
+  reporter: "mochawesome",
+  reporterOptions: {
+  charts: true,
+  overwrite: false,
+  html: false,
+  json: true,
+  reportDir: "cypress/reports"
+  }
+
 });
