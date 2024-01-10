5048d548729c56eb865ee4586620b9060d56a9b8
Mahabub Alam <mahabubalam.se2018@gmail.com>
2024-01-10 20:34:47 +0600

    update dynamic program

--- cypress/Pages/AdminPage.js
+++ cypress/Pages/AdminPage.js
@@ -6,9 +6,42 @@ export class AdminPage{
     current_language = ':nth-child(1) > .oxd-grid-2 > .oxd-grid-item > .oxd-input-group > :nth-child(2) > .oxd-select-wrapper > .oxd-select-text > .oxd-select-text-input'
     select_language = '.oxd-select-dropdown > :nth-child(4)'
     language_save_button = '.oxd-button'
+    add_employee_menu = '.oxd-topbar-body-nav > ul > :nth-child(3)'
+    firstname_input = '.--name-grouped-field > :nth-child(1) > :nth-child(2) > .oxd-input'
+    middlename_input = ':nth-child(2) > :nth-child(2) > .oxd-input'
+    lastname_input = ':nth-child(3) > :nth-child(2) > .oxd-input'
+    employee_id_input = '.orangehrm-full-width-grid > div > div > div:nth-child(2) > input'
+    credential_toggle_switch = '.oxd-switch-input'
+    username_input = ':nth-child(4) > .oxd-grid-2 > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input'
+    password_input = '.user-password-cell > .oxd-input-group > :nth-child(2) > .oxd-input'
+    confirm_password_input = '.oxd-grid-2 > :nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input'
+    profile_picture_input = '.orangehrm-employee-image > div > div:nth-child(2) > input'
+    add_employee_save_button = '.oxd-button--secondary'
+    calendar = ':nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-date-wrapper > .oxd-date-input > .oxd-icon'
+    month_dropdown = '.oxd-calendar-selector-month'
+    month = '.oxd-calendar-dropdown > :nth-child(1)'
+    year_dropdown = '.oxd-calendar-selector-year'
+    date_grid = ':nth-child(7) > .oxd-calendar-date'
+    employee_profile_save_button = ':nth-child(1) > .oxd-form > .oxd-form-actions > .oxd-button'
+    employee_list_menu = 'li.oxd-topbar-body-nav-tab:nth-child(2)'
+    employee_name_search_input = ':nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-autocomplete-wrapper > .oxd-autocomplete-text-input > input'
+    autocomplete_option = '.oxd-autocomplete-option'
+    search_button = '.oxd-form-actions > .oxd-button--secondary'
+    employeeIdOnList = '.oxd-table-card > .oxd-table-row > :nth-child(2) > div'
+    employeeNameOnList = '.oxd-table-card > .oxd-table-row > :nth-child(3) > div'
+    employeeLastnameOnList = '.oxd-table-card > .oxd-table-row > :nth-child(4) > div'
     //General variables
     expected_language = 'English (United States)'
     admin_url = 'https://opensource-demo.orangehrmlive.com/web/index.php/admin/viewSystemUsers'
+    firstname = 'Ethan'
+    middlename = 'James'
+    lastname = 'Hunt'
+    username = 'Ethan123'
+    password = 'Sunbeam1'
+    profile_picture_file = 'cypress/fixtures/profile.png'
+    selected_month = 'January'
+    year = '1995' 
+    date = '7'
 
     clickConfiguration(){
         cy.get(this.configuration_dropdown)
@@ -43,4 +76,143 @@ export class AdminPage{
     checkSelectedLanguage(){
         cy.get(this.current_language).should('have.text',this.expected_language)
     }
+
+    clickAddEmployee(){
+        cy.get(this.add_employee_menu)
+            .click();
+    }
+
+    enterFirstname(){
+        cy.get(this.firstname_input)
+            .type(this.firstname);
+    }
+
+    enterMiddlename(){
+        cy.get(this.middlename_input)
+            .type(this.middlename);
+    }
+
+    enterLastname(){
+        cy.get(this.lastname_input)
+            .type(this.lastname);
+    }
+
+    givenEmployeeId(){
+        return cy.get(this.employee_id_input)
+         .invoke('val');
+     }
+
+     clickCredentialToggleSwitch(){
+        cy.get(this.credential_toggle_switch)
+            .click()
+     }
+
+     enterUsername(){
+        cy.get(this.username_input)
+            .type(this.username)
+     }
+     enterPassword(){
+        cy.get(this.password_input)
+            .type(this.password)
+     }
+     enterConfirmPassword(){
+        cy.get(this.confirm_password_input)
+            .type(this.password)
+     }
+
+     uploadProfilePicture(){
+        cy.get(this.profile_picture_input)
+            .selectFile(this.profile_picture_file, {force: true})
+     }
+
+     clickAddEmployeeSaveButton(){
+        cy.get(this.add_employee_save_button)
+            .click()
+     }
+
+     clickCalendar(){
+        cy.get(this.calendar)
+            .click()
+     }
+
+     clickMonthDropdown(){
+        cy.get(this.month_dropdown)
+            .click()
+     }
+
+     selectMonth(){
+        cy.get(this.month)
+            .click()
+     }
+
+     clickYearDropdown(){
+        cy.get(this.year_dropdown)
+            .click()
+     }
+
+     selectYear(){
+        cy.contains(this.year)
+            .click()
+     }
+
+     selectDate(){
+        cy.get(this.date_grid)
+            .click()
+     }
+
+     clickEmployeeProfileSaveButton(){
+        cy.get(this.employee_profile_save_button)
+            .click()
+     }
+
+     checkEmployeeBirthMonth(){
+        cy.get(this.month_dropdown+"-selected > .oxd-text")
+            .should("have.text",this.selected_month)
+     }
+
+     checkEmployeeBirthYear(){
+        cy.get(this.year_dropdown+ '-selected > .oxd-text')
+            .should("have.text",this.year)
+     }
+     checkEmployeeBirthDate(){
+        cy.get(this.date_grid)
+            .should("have.text",this.date)
+     }
+
+     clickEmployeeListMenu(){
+        cy.get(this.employee_list_menu)
+            .click()
+     }
+
+     enterHintEmployeeName(){
+        cy.get(this.employee_name_search_input)
+            .type(this.firstname)
+     }
+
+     clickAutocompleteOption(){
+        cy.get(this.autocomplete_option)
+            .click()
+     }
+
+     clickSearchButton(){
+        cy.get(this.search_button)
+            .click()
+     }
+
+     checkEmployeeId(){
+        cy.get('@employeeId').then(employeeId=>{
+            cy.log(`Outside: ${employeeId}`)
+            cy.get(this.employeeIdOnList)
+                .should("have.text",employeeId)
+        });
+     }
+
+     checkEmployeeName(){
+        cy.get(this.employeeNameOnList)
+            .should('have.text',this.firstname+" "+this.middlename)
+     }
+     checkEmployeeLastname(){
+        cy.get(this.employeeLastnameOnList)
+            .should('have.text',this.lastname)
+     }
 }
--- cypress/e2e/OrangeHRM/admin.cy.js
+++ cypress/e2e/OrangeHRM/admin.cy.js
@@ -21,15 +21,14 @@ beforeEach(()=>{
 
 describe("Admin Module", ()=>{
     it("language should be change to English", ()=>{
-        cy.log("Test case 1 is started")
         cy.wait(4000)
-
         postLogin.clickAdminMenu();
         adminPage.clickConfiguration();
-        
         cy.wait(2000);
+
         adminPage.clickLocalization();
         cy.wait(4000);
+
         adminPage.displayedLanguage()
             .then((currentLanguage) =>{
                 if (currentLanguage.trim()!==adminPage.expected_language) {
@@ -41,93 +40,65 @@ describe("Admin Module", ()=>{
                     adminPage.checkSelectedLanguage();
                 }
             });
-        cy.log("Test case 1 is completed...")
     });
 });
 
 describe("PIM Module",()=>{
     let employeeId;
-    it("Add an Employee",()=>{
-        cy.get(':nth-child(2) > .oxd-main-menu-item')
-            .click()    // Go to PIM
-        cy.get('.oxd-topbar-body-nav > ul > :nth-child(3)')
-            .click()    // Click on Add Employee in top Navbar
-        cy.get('.--name-grouped-field > :nth-child(1) > :nth-child(2) > .oxd-input')
-            .type("Ethan")  //Enter First Name
-        cy.get(':nth-child(2) > :nth-child(2) > .oxd-input')
-            .type("James")  // Enter Middle Name
-        cy.get(':nth-child(3) > :nth-child(2) > .oxd-input')
-            .type("Parker") // Enter Last Name
+    it.only("Add an Employee",()=>{
+        postLogin.clickPimMenu()    // Go to PIM
+        adminPage.clickAddEmployee()    // Click on Add Employee in top Navbar
+        adminPage.enterFirstname()
+        adminPage.enterMiddlename()
+        adminPage.enterLastname()
 
-        cy.get('.orangehrm-full-width-grid > div > div > div:nth-child(2) > input').invoke('val').then(value=>{
+        adminPage.givenEmployeeId().then(value=>{
             employeeId = value;
             cy.log(`Employee ID: ${employeeId}`);
             cy.wrap(employeeId).as('employeeId');
         });
         
-        cy.get('.oxd-switch-input')
-            .click()    // Toggle for Create Login Details
-        cy.get(':nth-child(4) > .oxd-grid-2 > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input')
-            .type("Ethan123")   // Enter Username
-        cy.get('.user-password-cell > .oxd-input-group > :nth-child(2) > .oxd-input')
-            .type("Sunbeam1")   // Enter Password
-        cy.get('.oxd-grid-2 > :nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input')
-            .type("Sunbeam1")   // Enter Confirm Password
-        cy.get('.orangehrm-employee-image > div > div:nth-child(2) > input')
-            .selectFile("cypress/fixtures/profile.png", {force: true})
-        cy.get('.oxd-button--secondary')
-            .click()    // Click on Save button
+        adminPage.clickCredentialToggleSwitch()    // Toggle for Create Login Details
+        adminPage.enterUsername() 
+        adminPage.enterPassword()
+        adminPage.enterConfirmPassword()
+
+        adminPage.uploadProfilePicture()
+        adminPage.clickAddEmployeeSaveButton()    // Click on Save button
 
         cy.log("Input Date of Birth")
         cy.wait(10000)    
+
         cy.scrollTo(0,200)
-        cy.get(':nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-date-wrapper > .oxd-date-input > .oxd-icon')
-            .click()    //Open calendar
-        cy.get('.oxd-calendar-selector-month')
-            .click()
-        cy.get('.oxd-calendar-dropdown > :nth-child(1)')
-            .click()    //Month January Selected
-        cy.get('.oxd-calendar-selector-year')
-            .click()
-        cy.contains('1995')
-            .click()    //Year 1995 Selected
-        cy.get(':nth-child(7) > .oxd-calendar-date')
-            .click()    //Date 7 Selected
-        cy.get(':nth-child(1) > .oxd-form > .oxd-form-actions > .oxd-button')
-            .click()    //Save data
+        adminPage.clickCalendar()    //Open calendar
+        adminPage.clickMonthDropdown()
+        adminPage.selectMonth()    //Month January Selected
+        adminPage.clickYearDropdown()
+        adminPage.selectYear()    //Year 1995 Selected
+        adminPage.selectDate()   //Date 7 Selected
+        adminPage.clickEmployeeProfileSaveButton()    //Save data
         
         //Assertion for date of birth
         cy.scrollTo(0,200)
-        cy.get(':nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-date-wrapper > .oxd-date-input > .oxd-icon')
-            .click()
-        cy.get('.oxd-calendar-selector-month-selected > .oxd-text')
-            .should("have.text","January")
-        cy.get('.oxd-calendar-selector-year-selected > .oxd-text')
-            .should("have.text","1995")
-        cy.get('.--selected')
-            .should("have.text","7")
+        adminPage.clickCalendar()
 
+        adminPage.checkEmployeeBirthMonth()
+        adminPage.checkEmployeeBirthYear()
+        adminPage.checkEmployeeBirthDate()
         cy.wait(10000)
-        cy.get('li.oxd-topbar-body-nav-tab.--visited')
-            .click()
-        cy.get(':nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-autocomplete-wrapper > .oxd-autocomplete-text-input > input')
-            .type("Ethan")  //Enter name
+
+        adminPage.clickEmployeeListMenu()
+        adminPage.enterHintEmployeeName()  //Enter name
         cy.wait(2000)
-        cy.get('.oxd-autocomplete-option') //Click on Auto Completion
-            .click()
+
+        adminPage.clickAutocompleteOption()
         cy.wait(2000)    
-        cy.get('.oxd-form-actions > .oxd-button--secondary')
-            .click()    //Click on search button
-        cy.wait(2000)
-        cy.get('@employeeId').then(employeeId=>{
-            cy.log(`Outside: ${employeeId}`)
-            cy.get('.oxd-table-card > .oxd-table-row > :nth-child(2) > div')
-                .should("have.text",employeeId)
-        });
         
-        cy.get('.oxd-table-card > .oxd-table-row > :nth-child(3) > div')
-            .should("have.text", "Ethan James")
-        cy.get('.oxd-table-card > .oxd-table-row > :nth-child(4) > div')
-            .should("have.text", "Parker")
+        adminPage.clickSearchButton()
+        cy.wait(2000)
+        
+        adminPage.checkEmployeeId()   
+        adminPage.checkEmployeeName()
+        adminPage.checkEmployeeLastname()
     });
 });
